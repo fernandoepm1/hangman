6a9a6a9c2b147bed1d9966b3f017aa2464e3ffed
Fernando <Fernando@192.168.25.230>
2018-11-13 22:35:27 -0200

    Alterado o esquema de packages e classes do projeto

--- src/graphics/GameScreen.java
+++ src/graphics/GameScreen.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package core;
+package graphics;
 
 /**
  *
  * @author alunoti
  */
-public class WordGenerator {
+public class GameScreen {
     
 }
